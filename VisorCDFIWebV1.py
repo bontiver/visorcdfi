4a0571762e6be468470516a2f5b207c68d8c0689
bontiver <bontiver@gmail.com>
2024-08-24 08:29:26 -0700

    Se le agregaron consultas de claves

--- VisorCDFIWebV1.py
+++ VisorCDFIWebV1.py
@@ -55,7 +55,7 @@ def parse_cfdi(xml_file):
         "Fecha": root.attrib.get('Fecha', ''),
         "Nombre Emisor": root.find('cfdi:Emisor', ns).attrib.get('Nombre', ''),
         "Rfc Emisor": root.find('cfdi:Emisor', ns).attrib.get('Rfc', ''),
-        "RegimenFiscalEmisor": regimen_fiscal_emisor,  # Agregado debajo de Rfc Emisor
+        "RegimenFiscalEmisor": regimen_fiscal_emisor,
         "MetodoPago": root.attrib.get('MetodoPago', ''),
         "FormaPago": root.attrib.get('FormaPago', ''),
         "UsoCFDI": root.find('cfdi:Receptor', ns).attrib.get('UsoCFDI', ''),
@@ -79,8 +79,72 @@ def format_currency(value):
     except (ValueError, TypeError):
         return value
 
+def search_clave_prod_serv(query, search_type):
+    conn = sqlite3.connect('ClaveProdSat.db')
+    cursor = conn.cursor()
+
+    if not query:
+        cursor.execute("SELECT ClaveProdServ, Descripcion FROM c_ClaveProdServ")
+    elif search_type == "Clave":
+        cursor.execute("SELECT ClaveProdServ, Descripcion FROM c_ClaveProdServ WHERE ClaveProdServ = ?", (query,))
+    else:
+        cursor.execute("SELECT ClaveProdServ, Descripcion FROM c_ClaveProdServ WHERE Descripcion LIKE ?", ('%' + query + '%',))
+
+    results = cursor.fetchall()
+    conn.close()
+
+    return results
+
+def search_uso_cfdi(query, search_type):
+    conn = sqlite3.connect('c_UsoCFDI.db')
+    cursor = conn.cursor()
+
+    if not query:
+        cursor.execute("SELECT c_UsoCFDI, Descripcion FROM c_UsoCFDI")
+    elif search_type == "Clave":
+        cursor.execute("SELECT c_UsoCFDI, Descripcion FROM c_UsoCFDI WHERE c_UsoCFDI = ?", (query,))
+    else:
+        cursor.execute("SELECT c_UsoCFDI, Descripcion FROM c_UsoCFDI WHERE Descripcion LIKE ?", ('%' + query + '%',))
+
+    results = cursor.fetchall()
+    conn.close()
+
+    return results
+
+def search_forma_pago(query, search_type):
+    conn = sqlite3.connect('c_FormaPago.db')
+    cursor = conn.cursor()
+
+    if not query:
+        cursor.execute("SELECT c_FormaPago, Descripcion FROM c_FormaPago")
+    elif search_type == "Clave":
+        cursor.execute("SELECT c_FormaPago, Descripcion FROM c_FormaPago WHERE c_FormaPago = ?", (query,))
+    else:
+        cursor.execute("SELECT c_FormaPago, Descripcion FROM c_FormaPago WHERE Descripcion LIKE ?", ('%' + query + '%',))
+
+    results = cursor.fetchall()
+    conn.close()
+
+    return results
+
+def search_regimen_fiscal(query, search_type):
+    conn = sqlite3.connect('c_RegimenFiscal.db')
+    cursor = conn.cursor()
+
+    if not query:
+        cursor.execute("SELECT c_RegimenFiscal, Descripcion FROM c_RegimenFiscal")
+    elif search_type == "Clave":
+        cursor.execute("SELECT c_RegimenFiscal, Descripcion FROM c_RegimenFiscal WHERE c_RegimenFiscal = ?", (query,))
+    else:
+        cursor.execute("SELECT c_RegimenFiscal, Descripcion FROM c_RegimenFiscal WHERE Descripcion LIKE ?", ('%' + query + '%',))
+
+    results = cursor.fetchall()
+    conn.close()
+
+    return results
+
 def main():
-    st.title("Visor de CFDI")
+    st.title("Visor de CFDI y Buscador de ClaveProdServ/UsoCFDI/Forma de Pago/Régimen Fiscal")
 
     st.markdown(
         """
@@ -94,6 +158,63 @@ def main():
         """, unsafe_allow_html=True
     )
 
+    # Sidebar para la búsqueda de ClaveProdServ, UsoCFDI, Forma de Pago y Régimen Fiscal
+    with st.sidebar:
+        st.write("### Búsquedas en ClaveProdServ, UsoCFDI, Forma de Pago y Régimen Fiscal")
+
+        with st.expander("Desplegar Búsqueda ClaveProdServ"):
+            search_query = st.text_input("Ingresa la clave o una descripción para buscar en el catálogo de ClaveProdServ:")
+            search_type = st.radio("Tipo de búsqueda:", ('Clave', 'Descripción'))
+
+            if st.button("Buscar ClaveProdServ"):
+                results = search_clave_prod_serv(search_query, search_type)
+                if results:
+                    df_results = pd.DataFrame(results, columns=["ClaveProdServ", "Descripción"])
+                    st.write("### Resultados de la búsqueda:")
+                    st.table(df_results)
+                else:
+                    st.warning("No se encontraron resultados para la búsqueda en ClaveProdServ.")
+
+        with st.expander("Desplegar Búsqueda UsoCFDI"):
+            search_query_cfdi = st.text_input("Ingresa la clave o una descripción para buscar en el catálogo de UsoCFDI:")
+            search_type_cfdi = st.radio("Tipo de búsqueda UsoCFDI:", ('Clave', 'Descripción'))
+
+            if st.button("Buscar UsoCFDI"):
+                results_cfdi = search_uso_cfdi(search_query_cfdi, search_type_cfdi)
+                if results_cfdi:
+                    df_results_cfdi = pd.DataFrame(results_cfdi, columns=["c_UsoCFDI", "Descripción"])
+                    st.write("### Resultados de la búsqueda en UsoCFDI:")
+                    st.table(df_results_cfdi)
+                else:
+                    st.warning("No se encontraron resultados para la búsqueda en UsoCFDI.")
+
+        with st.expander("Desplegar Búsqueda Forma de Pago"):
+            search_query_fp = st.text_input("Ingresa la clave o una descripción para buscar en el catálogo de Forma de Pago:")
+            search_type_fp = st.radio("Tipo de búsqueda Forma de Pago:", ('Clave', 'Descripción'))
+
+            if st.button("Buscar Forma de Pago"):
+                results_fp = search_forma_pago(search_query_fp, search_type_fp)
+                if results_fp:
+                    df_results_fp = pd.DataFrame(results_fp, columns=["c_FormaPago", "Descripción"])
+                    st.write("### Resultados de la búsqueda en Forma de Pago:")
+                    st.table(df_results_fp)
+                else:
+                    st.warning("No se encontraron resultados para la búsqueda en Forma de Pago.")
+
+        with st.expander("Desplegar Búsqueda Régimen Fiscal"):
+            search_query_rf = st.text_input("Ingresa la clave o una descripción para buscar en el catálogo de Régimen Fiscal:")
+            search_type_rf = st.radio("Tipo de búsqueda Régimen Fiscal:", ('Clave', 'Descripción'))
+
+            if st.button("Buscar Régimen Fiscal"):
+                results_rf = search_regimen_fiscal(search_query_rf, search_type_rf)
+                if results_rf:
+                    df_results_rf = pd.DataFrame(results_rf, columns=["c_RegimenFiscal", "Descripción"])
+                    st.write("### Resultados de la búsqueda en Régimen Fiscal:")
+                    st.table(df_results_rf)
+                else:
+                    st.warning("No se encontraron resultados para la búsqueda en Régimen Fiscal.")
+
+    # Sección para cargar y visualizar CFDI
     uploaded_file = st.file_uploader("Carga tu archivo XML de CFDI", type="xml")
 
     if uploaded_file is not None:
